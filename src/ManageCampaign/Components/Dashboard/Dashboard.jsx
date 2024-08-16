51799717136f19ed523221c6a585d79657c12baf
Darshana1907 <darshanakaur1907@gmail.com>
2024-08-16 10:54:35 +0530

    dashbord ready

--- src/ManageCampaign/Components/Dashboard/Dashboard.jsx
+++ src/ManageCampaign/Components/Dashboard/Dashboard.jsx
@@ -1,6 +1,15 @@
 import React, { useContext, useState } from "react";
 import { Mycontext } from "../../../utils/Context";
 import { Link, useLocation } from "react-router-dom";
+import RoiComp from "./RofReport/Rofreport";
+
+
+
+import InvestComp from "./InvestComp/InvestComp";
+import CampaignComponent from "./CampaignDetail/CampaignComponent";
+import BarGraph from "./BarGraph/BarGraph";
+
+import PieChartComponent from "./Piechart/PieChartComponent";
 
 const Dashboard = () => {
   const contextState = useContext(Mycontext);
@@ -11,13 +20,27 @@ const Dashboard = () => {
 
   return (
     <div
-      class={` flex relative ${
+      class={` flex h-screen relative ${
         !expanded
           ? "left-[100px] w-[calc(100%-110px)]"
           : "left-[320px] w-[calc(100%-320px)]"
-      }  overflow-y-auto  bg-white space-y-4 p-4 `}
+      }  overflow-y-auto   space-y-4 p-4 `}
     >
-       dashboard....
+     
+       <div className="flex flex-col justify-between ml-8 mt-2 mb-2  gap-4">
+        <CampaignComponent/>
+        <PieChartComponent/>
+       </div>
+       <div class="flex flex-col w-10/12 ml-10 mr-4 top-2/4 ">
+        
+        <BarGraph expanded={expanded}/>
+        <RoiComp expanded={expanded}/>
+        <InvestComp expanded={expanded} />
+       </div>
+
+      
+
+       <div></div>
     </div>
   );
 };
